ad20302e03bbbc2f98832a833a11de4eb577a161
nicholascarlino <nicholascarlino@gmail.com>
2015-10-20 10:02:39 -0500

    intro

--- intro.js
+++ intro.js
@@ -1,7 +1,7 @@
-var i = 0;
-var text;
 var introState = {
 	create: function(){
+		i = 0;
+		counter = 0;
 		message1 = "One day... in a far off place...";
 		message2 = "You were sailing on a boat.  Your own boat.  One you had crafted yourself";
 		message3 = "Suddenly, a storm hit";
@@ -9,15 +9,31 @@ var introState = {
 		message5 = "You were scared at first... but then calmed down";
 		message6 = "You decided to try to find some food and water: enter to start";
 		messages = [message1, message2, message3, message4, message5, message6];
+	texts = new Array(6);
 	var entkey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 	entkey.onDown.addOnce(this.start, this);
-	text = game.add.text(game.width/2 - 200, game.height/2 - 200, messages[i], {font: "50px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 400, align: "left"});
+	var style = {font: "30px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 600, align: "center"};
+	prompt = game.add.text(25, 400, messages[0], style);
+	i++;
+	game.time.events.loop(Phaser.Timer.SECOND * 3, this.createText, this);
 	},
 	update: function(){
-		text.kill();
-		text = game.add.text(game.width/2 - 200, game.height/2 - 200, messages[i], {font: "50px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 400, align: "left"});
+		prompt.y -=1;
+		for (var i = 0; i < 5; i++){
+			if (texts[i]){
+				texts[i].y -= 1;
+			}
+		}
+	
 	},
 	start: function(){
 		game.state.start('lvl1_d');
+	},
+	createText: function(){
+		if(i < 6){
+			var style = {font: "30px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 600, align: "center"};
+			texts[i] = game.add.text(25, 400, messages[i], style);
+			i++;
+		}
 	},
 }
