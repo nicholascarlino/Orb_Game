cbb03a94bb64729280e16c339b222bade0c5444f
rohundhar <rohundhar@gmail.com>
2015-10-20 12:36:36 -0400

    updated level3

--- lvl3_d.js
+++ lvl3_d.js
@@ -3,12 +3,9 @@ var level3DayState = {
 		// START MAP CONSTRUCTION
 
 		game.map = game.add.tilemap('Level3Day');
-		console.log("after MAP 1");
 		// ADDS THE TILE SETS INTO THE MAP OBJECT
 		game.map.addTilesetImage('completeSet', 'CompleteTile');
-		console.log("after MAP 2");
 		game.map.addTilesetImage('Collision3', 'Collisiontile'); //check
-		console.log("after MAP 3");
 
 		// DRAW DIFFERENT LAYERS OF MAP
 		background = game.map.createLayer('Background3');
@@ -43,7 +40,7 @@ game.player = new Player(game, 1760, 1792, 8);
 
 		background3 = game.map.createLayer('Mausoleum');
 		background4 = game.map.createLayer('Collision');
-		background4.alpha = .4;
+		background4.alpha = 0;
 
 		// DEFINE COLLISION OBJECTS IN MAP
 		game.map.setCollision(3825, true, 'Collision');
@@ -105,21 +102,18 @@ game.player = new Player(game, 1760, 1792, 8);
 		game.camera.follow(game.player);
 	},
 	createBlueDragons: function(){
-		console.log("creating blue dragons");
 		 for (var i = 0; i < 3; i++){
 			var enemy = new Enemy(game, 832, 1280, 'dragon');
 			game.enemies.add(enemy);
 		  }
 	},
 	createGoldDragons: function(){
-		console.log("creating gold dragons");
 		 for (var i = 0; i < 2; i++){
 			var enemy = new Enemy(game, 1728, 256, 'GoldDragon');
 			game.enemies.add(enemy);
 		  }
 	},
 	createGolem: function(){
-		console.log("creating golems");
 		 for (var i = 0; i < 2; i++){
 		 	console.log(i);
 			var enemy = new Enemy(game, 128, 128, 'golem');
