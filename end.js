a58f207bf0dea79e4024949a07401ba2a5c9916f
sosena <sosena.bekele@tufts.edu>
2015-10-20 09:46:32 -0400

    did end

--- end.js
+++ end.js
@@ -1,6 +1,7 @@
 var gameOverState = {
 	create: function() {
-		var i = 0;
+		 i = 0;
+		 counter = 0;
 		if (npc_is_attached == true){
 			message1 = "Out of the Obelisk fell a glowing orb...";
 			message2 = "It shined with a green light... A haunting light...";
@@ -16,24 +17,46 @@ var gameOverState = {
 			message2 = "It shined with a green light... A haunting light...";
 			message3 = "You pick it up off the ground and feel a strange energy course through you...";
 			message4 = "You smile as you think of the possiblities...";
-			message5 = "END";
+			message5 = "                END                        ";
 			message6 = "To restart, hit enter";
 			messages = [message1, message2, message3, message4, message5, message6];
 		}
+		texts = new Array(6);
+		action = game.input.keyboard.addKey(Phaser.Keyboard.L);
 		var entkey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-
+		var style = { font: "30px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 600, align: "center" };
+		
+		prompt = game.add.text(75, 400, messages[0], style);
+		i++;
+	    game.time.events.loop(Phaser.Timer.SECOND * 3 , this.createText, this);
 		entkey.onDown.addOnce(this.restart, this);
 
 	},
 	update: function() {
-		var text = game.add.text(game.width / 2, game.height / 2, messages[i], {font: "50px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 400, align: "left" });
 			if (action.isDown){
+				console.log("IN HERE");
 				i++;
 			}
+			prompt.y -=1;
+			for( var i = 0 ; i < 5 ; i++){
+				if(texts[i]){
+					texts[i].y -= 1;
+				}
+			}
+		
 
 	},
 	restart: function() {
 		game.state.start('menu');
+	},
+	createText: function() {
+		if(i < 6){
+	
+		console.log("creating Text...")
+		var style = { font: "30px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 600, align: "center" };
+		texts[i] = game.add.text(75, 400, messages[i], style);
+		i++;
+	   }
 	},
 }	
 
